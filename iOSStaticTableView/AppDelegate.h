e50a3f5270614c96c0610c61e084ab967e1c761d
kraken <alexpadillarosas@gmail.com>
2020-08-29 23:58:42 +1000

    Initial commit

---
+++ iOSStaticTableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSStaticTableView
+//
+//  Created by alex on 29/8/20.
+//  Copyright © 2020 alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
